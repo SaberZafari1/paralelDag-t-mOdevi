e6c067b270f601d08998ccded5a3730b04b35564
SaberZafari1 <sazaf.1425@gmail.com>
2025-05-02 11:17:22 +0300

    2. ve 3. projelerin kodlari

---
+++ proje2.sql
@@ -0,0 +1,51 @@
+﻿--2. Veritabanı Yedekleme ve Felaketten Kurtarma Planı
+--Tam, Artık, Fark Yedeklemeleri: 
+--Tam Yedekleme: Veritabanının tüm verisini yedekler.
+BACKUP DATABASE AdventureWorks2019
+TO DISK = 'C:\SQLYedek\Adventureworks2019_TamYedek.bak'
+WITH FORMAT, INIT,
+NAME = 'Adventureworks2019 Tam Yedekleme';
+
+--Fark Yedekleme: En son tam yedekten bu yana değişen verileri yedekler. Geri yükleme için hem tam hem fark yedeği gerekir.
+BACKUP DATABASE AdventureWorks2019
+TO DISK = 'C:\SQLYedek\AdventureWorks2019_DIFF.bak'
+WITH DIFFERENTIAL,
+NAME = 'Adventureworks2019 Fark Yedekleme';
+
+--Artırımlı (Transaction Log) Yedekleme: Veritabanındaki işlemleri (logları) yedekleyerek veri kaybını en aza indirir.
+ALTER DATABASE AdventureWorks2019
+SET RECOVERY FULL;
+GO
+
+BACKUP LOG AdventureWorks2019
+TO DISK = 'C:\SQLYedek\Adventureworks2019_LOG.trn'
+WITH NAME = 'Adventureworks2019 LOG Yedegi';
+
+--Zamanlayıcılarla Yedekleme:
+--SQL Server Agent üzerinden “New Job” tanımlanarak günlük/tarifeli yedek alma işlemleri yapılmıştır. Maintenance Plan Wizard ile Full, Differential ve Log yedekleri zamanlanmıştır.
+
+--Felaketten Kurtarma Senaryoları:
+--FULL
+--Back up Database ile ayarlama yapılmış ve aşağıdaki kodla tamamlanmıştır.
+BACKUP DATABASE [AdventureWorks2019] TO DISK = 'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\Backup\AdventureWorks2019Full.bak'
+
+--Differential
+--Back up Database ile ayarlama yapılmış
+
+--Transaction Log
+--Back up Database ile ayarlama yapılmış
+
+
+--Test Yedekleme Senaryoları:
+RESTORE VERIFYONLY
+FROM DISK = 'C:\SQLYedek\Adventureworks2019_TamYedek2.bak';
+
+RESTORE VERIFYONLY 
+FROM DISK = 'C:\SQLYedek\Adventureworks2019_DIFF2.bak';
+
+RESTORE VERIFYONLY 
+FROM DISK = 'C:\SQLYedek\Adventureworks2019_LOG2.trn'
+
+
+
+--Merve nur dogru
---
+++ proje3.sql
@@ -0,0 +1,60 @@
+﻿--Veritabanı Güvenliği ve Erişim Kontrolü
+--Erişim Yönetimi:
+--Bu işlem, SQL Server Management Studio (SSMS) arayüzü üzerinden "Logins" > "New Login" yoluyla yapılmıştır. SQL Server authentication seçeneğinden limit2 adı verilerek şifre oluşturuldu. 
+--Server Roles kısmından kullanıcıya public belirlendi. Daha sonrasında ise İlgili veritabanı üzerinde kullanıcıya sınırlı erişim tanımlandı.Tablo düzeyinde okuma/yazma izinleri kısıtlandı. 
+
+
+--Veri Şifreleme
+--Sertifika oluşturuluyor
+CREATE CERTIFICATE TDE_Cert
+WITH SUBJECT = 'TDE Sertifikası';
+
+--AdventureWorks2019 üzerinde şifreleme anahtarı oluşturuluyor
+USE AdventureWorks2019;
+CREATE DATABASE ENCRYPTION KEY 
+WITH ALGORITHM = AES_256
+ENCRYPTION BY SERVER CERTIFICATE TDE_Cert;
+
+--Şifreleme aktif ediliyor
+ALTER DATABASE AdventureWorks2019 SET ENCRYPTION ON;
+
+--Doğrulama
+SELECT name, is_encrypted
+FROM sys.databases
+WHERE name = 'AdventureWorks2019'
+
+
+--SQL Injection Testleri:
+--Normal sorgu
+SELECT BusinessEntityID, FirstName, LastName
+FROM Person.Person
+Where BusinessEntityID = 1;
+
+--SQL injection testi (güvensiz örnek)
+SELECT BusinessEntityID, FirstName, LastName
+FROM Person.Person
+WHERE BusinessEntityID = 1 OR 1=1;
+
+
+--Audit Logları:
+--Audit nesnesi oluşturuluyor
+USE master;
+CREATE SERVER AUDIT Audit_AdventureWorks2019
+TO FILE
+(
+	FILEPATH = 'C:\SQLAudit\'
+)
+WITH 
+(
+	ON_FAILURE = CONTINUE
+)
+GO 
+ALTER SERVER AUDIT Audit_AdventureWorks2019
+WITH (State = ON);
+GO
+
+--Audit aktif hale getiriliyor
+ALTER SERVER AUDIT Audit_AdventureWorks2019
+WITH (STATE = ON);
+
+--Merve Nur Dogru
